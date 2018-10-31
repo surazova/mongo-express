cd2641924701eba7595ea7a99bba3cb5c28ef064
Sitora Urazova <sitoraurazova@gmail.com>
2018-10-31 17:31:01 +0000

    Implemented put route

--- routes/api/users.js
+++ routes/api/users.js
@@ -15,7 +15,7 @@ router.get('/', (req,res) => {
 //url /api/users/?name=sitora   (query)
 //url /api/users/sitora?comment   (params)
 router.get('/:name', (req, res) =>{
-  const { name }= req.params.name //use destructuring {name, password, avatar} and then just {name}
+  const  name = req.params.name //use destructuring {name, password, avatar} and then just {name}
   User.findOne({ name })  //set to object
   .then(user => {
     if(!user) {
@@ -35,9 +35,67 @@ router.post('/', (req,res) => {
   })
   //newUser.avatar = avatar 
   
-  newUser.save()
+  newUser.save()  //asynchronous method
+    .then(user => res.status(201).json(user))  //when you save, you get the id of the new user, and the date 
+    .catch(err => {
+      res.status(500)
+        .json({
+          status: "error", 
+          message: err
+        });
+    })
 })
 
+router.delete('/:name', (req,res) => {
+  // TODO: Procted route, make sure the user is the one deleting, not just anyone 
+
+
+const name = req.params.name //use destructuring {name, password, avatar} and then just {name}
+  User.findOne({ name })  //set to object
+  .then(user => {
+    if(!user) {
+      return res.status(404).json({message: `User: ${name} not found`})
+    }
+    user.remove()
+      .then(() => res.status(404).json({message:` user ${name} successfully deleted`}))
+      .catch(err => res.status(500).json(err));
+})
+
+});
+
+
+
+router.put('/:name', (req, res) => { //a put works similerly to a post, you still need parameters. 
+  const name = req.params.name
+  User.findOne({name})
+  .then(user => {
+    if(!user) {
+      return res.status(409).json({message: `user ${name} already exists`})
+    } else {  //do I need an else? 
+  if(user => {
+  const {name, password, avatar} = req.body;
+  const newUser = new User({
+    name, 
+    password,
+    avatar
+  
+  }
+  )
+    newUser.save()  //asynchronous method
+    .then(user => res.status(201).json(user))  //when you save, you get the id of the new user, and the date 
+    .catch(err => {
+      res.status(500)
+        .json({
+          status: "error", 
+          message: err
+        });
+    })
+})
+
+  //   User.findOneAndUpdate({name})
+  //     .then(() => res.status(204).json({message: `user ${name} successfully updated`}))
+  //     .catch(() => res.status(500).json(err));
+  
+  // })
 
 module.exports = router;
-
--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const mongoose = require('mongoose');
 //keys
 const db = require('./config/keys').mongoURI; //grabbing the mongo key 
 
-const app = express();
-
 mongoose  //connect to the db in a synchronous way
     .connect(db)
     .then(() => console.log("mongoDB connected"))
     .catch(err =>console.log(err))
-    
+
+const app = express();
+
 //use body-parser
 app.use(bodyParser.urlencoded({extended: false})) 
 app.use(bodyParser.json()) //parser urlencoded, not the extended, and then return the data in json format 
@@ -24,12 +24,10 @@ app.get('/', (req, res) => res.send(`<h1>Hello</h1>`))
 
 
 
-
-
-
 //Port
 const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`App listening on port ${port}`))
+
 
 
